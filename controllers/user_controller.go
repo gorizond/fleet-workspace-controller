bbe088b8ece0a4b7a36f6e5650a546d1b263ec67
Negash <i@negash.ru>
2025-05-05 04:26:11 +0500

    change to DisplayName

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -2,64 +2,64 @@ package controllers
 
 import (
 	"context"
-	"strings"
 	"github.com/rancher/lasso/pkg/log"
 	managementv3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
 	"github.com/rancher/rancher/pkg/generated/controllers/management.cattle.io"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	
 )
 
 func InitUserController(ctx context.Context, mgmt *management.Factory) {
 	users := mgmt.Management().V3().User()
 	users.OnChange(ctx, "gorizond-user-controller", func(key string, obj *managementv3.User) (*managementv3.User, error) {
-			if obj == nil {
-				return nil, nil
-			}
-			// check non system users
-			if obj.Status.Conditions == nil {
-				return nil, nil
-			}
-			// check workspace init on user create
-			firstInit := obj.Annotations != nil && obj.Annotations["self-workspace-init"] == "true"
+		if obj == nil {
+			return nil, nil
+		}
+		// check non system users
+		if obj.Status.Conditions == nil {
+			return nil, nil
+		}
+		// check workspace init on user create
+		firstInit := obj.Annotations != nil && obj.Annotations["self-workspace-init"] == "true"
 
-			if firstInit {
-				return obj, nil
-			}
-			// ignore system users
-			for _, id := range obj.PrincipalIDs {
-				if strings.HasPrefix(id, "system://") {	
-					if obj.Annotations == nil {
-						obj.Annotations = make(map[string]string)
-					}
-					obj.Annotations["self-workspace-init"] = "true"
-					return users.Update(obj)
+		if firstInit {
+			return obj, nil
+		}
+		// ignore system users
+		for _, id := range obj.PrincipalIDs {
+			if strings.HasPrefix(id, "system://") {
+				if obj.Annotations == nil {
+					obj.Annotations = make(map[string]string)
 				}
+				obj.Annotations["self-workspace-init"] = "true"
+				return users.Update(obj)
 			}
+		}
 
-			fleetworkspace := &managementv3.FleetWorkspace{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: obj.Username + "-workspace",
-					Annotations: map[string]string{
-						"field.cattle.io/creatorId": obj.Name,
-					},
+		fleetworkspace := &managementv3.FleetWorkspace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: obj.DisplayName + "-workspace",
+				Annotations: map[string]string{
+					"field.cattle.io/creatorId": obj.Name,
 				},
-			}
+			},
+		}
 
-			_, err := mgmt.Management().V3().FleetWorkspace().Create(fleetworkspace)
-			if err != nil && !errors.IsAlreadyExists(err) {
-				log.Infof("Failed to create fleetworkspace %s: %v", obj.Username, err)
-			}
-			obj = obj.DeepCopy()
+		_, err := mgmt.Management().V3().FleetWorkspace().Create(fleetworkspace)
+		if err != nil && !errors.IsAlreadyExists(err) {
+			log.Infof("Failed to create fleetworkspace %s: %v", obj.Username, err)
+		}
+		obj = obj.DeepCopy()
 
-			if obj.Annotations == nil {
-				obj.Annotations = make(map[string]string)
-			}
+		if obj.Annotations == nil {
+			obj.Annotations = make(map[string]string)
+		}
 
-			obj.Annotations["self-workspace-init"] = "true"
+		obj.Annotations["self-workspace-init"] = "true"
 
-			// Here we are using the k8s client embedded onto the users controller to perform an update. This will go to the K8s API.
-			return users.Update(obj)
-		},
+		// Here we are using the k8s client embedded onto the users controller to perform an update. This will go to the K8s API.
+		return users.Update(obj)
+	},
 	)
 }
